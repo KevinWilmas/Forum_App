abee09dd0e6e5fc35a0615bd3c53a4b07a57fc27
kevinwilmas <kevinwilmas@outlook.com>
2023-02-16 17:57:41 -0500

    Page Home Added, JSON data, and CSS

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
-<script setup></script>
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import PageHome from "./components/PageHome.vue";
 
-<template></template>
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    PageHome,
+  },
+};
+</script>
+
+<template>
+  <!-- <hello-world></hello-world> -->
+  <page-home></page-home>
+</template>
 
 <style scoped></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,16 @@
-<script setup>
-data = {
-  categories: [
-    {
-      id: "c1",
-      name: "Chats",
-      forums: ["f1"],
-    },
-  ],
-  forums: [
-    {
-      id: "f1",
-      name: "fishing",
-      description: "lets talk fishing",
-      categoryId: "c1",
-      threads: ["t1"],
-    },
-  ],
-  users: [
-    {
-      id: "u1",
-      name: "Alex",
-    },
-  ],
-  threads: [
-    {
-      id: "t1",
-      title: "What is your favorite food?",
-      publishedAt: 1698983092,
-      posts: [],
-      userId: "u1",
-    },
-  ],
-  posts: [
-    {
-      id: "p1",
-      publishedAt: 1698983092,
-      userId: "u1",
-      text: "I like pizza, and you?",
-      threadId: "t1",
-    },
-  ],
+<script>
+import sourceData from "../data.json";
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+    };
+  },
 };
-
-data.posts.filter((post) => post.userId === "u1");
-thread = data.threads.find((t) => t.id === "t1");
-threadPosts = data.posts.filter((p) => p.threadId === thread.id);
 </script>
 
-<template></template>
+<template>
+  <div>{{ threads }}</div>
+</template>
 
 <style scoped></style>
