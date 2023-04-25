1dc3708139a35e2acd1245d651fb7defddd47371
kevinwilmas <kevinwilmas@outlook.com>
2023-04-25 14:22:59 -0500

    Working on createThread in Threadstore (unfinished)

--- src/components/stores/PostsStore.js
+++ src/components/stores/PostsStore.js
@@ -11,6 +11,14 @@ export const usePostsStore = defineStore("PostsStore", {
   },
   getters: {},
   actions: {
+    // **VUEX**
+    // createPost ({ commit, state }, post) {
+    //   post.id = 'ggqq' + Math.random()
+    //   post.userId = state.authId
+    //   post.publishedAt = Math.floor(Date.now() / 1000)
+    //   commit('setPost', { post }) // set the post
+    //   commit('appendPostToThread', { postId: post.id, threadId: post.threadId }) // append post to thread
+    // },
     createPost(post) {
       post.id = "ggqq" + Math.random();
       const authUsersStore = useAuthUsersStore();
--- src/components/stores/ThreadsStore.js
+++ src/components/stores/ThreadsStore.js
@@ -1,10 +1,35 @@
 import { defineStore } from "pinia";
 import sourceData from "@/data.json";
 
+import { usePostsStore } from "./PostsStore";
+import { useAuthUsersStore } from "./AuthUsersStore";
+import { useForumsStore } from "./ForumsStore";
+
 export const useThreadsStore = defineStore("ThreadsStore", {
   state: () => {
     return { threads: sourceData.threads };
   },
   getters: {},
-  actions: {},
+  actions: {
+    // **VUEX**
+    // createThread ({ commit, state, dispatch }, { text, title, forumId }) {
+    //   const id = 'ggqq' + Math.random()
+    //   const userId = state.authId
+    //   const publishedAt = Math.floor(Date.now() / 1000)
+    //   const thread = { forumId, title, publishedAt, userId, id }
+    //   commit('setThread', { thread })
+    //   commit('appendThreadToUser', { userId, threadId: id })
+    //   commit('appendThreadToForum', { forumId, threadId: id })
+    //   dispatch('createPost', { text, threadId: id })
+    // },
+    createThread(threadForm) {
+      threadForm.id = "ggqq" + Math.random();
+      const postsStore = usePostsStore();
+      const forumsStore = useForumsStore();
+      const authUsersStore = useAuthUsersStore();
+      threadForm.userId = authUsersStore.authId;
+      threadForm.publishedAt = Math.floor(Date.now() / 1000);
+      threadForm.thread = { forumId, title, publishedAt, userId, id };
+    },
+  },
 });
