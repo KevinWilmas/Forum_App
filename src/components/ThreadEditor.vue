d94885c886bbdaa28d62619b3256cfc6c9e3fd4e
kevinwilmas <kevinwilmas@outlook.com>
2023-05-05 08:16:06 -0500

    almost working Thread Editor component

---
+++ src/components/ThreadEditor.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from "vue";
+
+const title = ref("");
+const text = ref("");
+
+const emit = defineEmits(["save", "cancel"]);
+
+function save() {
+  emit("save", { title: title.value, text: text.value });
+}
+</script>
+
+<template>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <label for="thread_title">Title:</label>
+      <input
+        v-model="title"
+        type="text"
+        id="thread_title"
+        class="form-input"
+        name="title"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="thread_content">Content:</label>
+      <textarea
+        v-model="text"
+        id="thread_content"
+        class="form-input"
+        name="content"
+        rows="8"
+        cols="140"
+      ></textarea>
+    </div>
+
+    <div class="btn-group">
+      <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
+      <button class="btn btn-blue" type="submit" name="Publish">Publish</button>
+    </div>
+  </form>
+</template>
+
+<style scoped></style>
--- src/pages/ThreadCreate.vue
+++ src/pages/ThreadCreate.vue
@@ -16,14 +16,11 @@ const forum = computed(() =>
   forums.value.find((forum) => forum.id === props.forumId)
 );
 
-const title = ref("");
-const text = ref("");
-
-async function save() {
+async function save(title, text) {
   const thread = await createThread({
     forumId: forum.value.id,
-    title: title.value,
-    text: text.value,
+    title,
+    text,
   });
   router.push({ name: "ThreadShow", params: { id: thread.id } });
 }
@@ -37,38 +34,7 @@ function cancel() {
     <h1>
       Create new thread in <i>{{ forum.name }}</i>
     </h1>
-
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input
-          v-model="title"
-          type="text"
-          id="thread_title"
-          class="form-input"
-          name="title"
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea
-          v-model="text"
-          id="thread_content"
-          class="form-input"
-          name="content"
-          rows="8"
-          cols="140"
-        ></textarea>
-      </div>
-
-      <div class="btn-group">
-        <button @click="cancel" class="btn btn-ghost">Cancel</button>
-        <button class="btn btn-blue" type="submit" name="Publish">
-          Publish
-        </button>
-      </div>
-    </form>
+    <ThreadEditor @save="save" @cancel="cancel" />
   </div>
 </template>
 
